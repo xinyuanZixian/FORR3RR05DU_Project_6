86a29dc81b5c0af9ff22faf0ad7c46497e14e6d7
xinyuanZixian <saikiu1996@hotmail.com>
2019-04-09 12:47:53 +0000

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,12 @@
+triangle = open("p067_triangle.txt","r")
+tree = [[int(y) for y in x.rstrip("\n").split(" ")] for x in triangle]
+cache = []
+
+def maximumPathSum(tree, depth):
+    i = 0
+    #while i >= len(tree[depth]) - 1:
+        #d = max(tree[depth][i] + tree[depth - 1][i], tree[depth][i + 1] + tree[depth - 1][i])
+    return max(tree[depth][i] + tree[depth - 1][i], tree[depth][i + 1] + tree[depth - 1][i])
+
+
+print(maximumPathSum(tree, len(tree) - 1))
